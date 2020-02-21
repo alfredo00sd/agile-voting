bb3aefe61649223d60201044a8f4a8644c171db0
carlsosa <crsaapp@gmail.com>
2020-02-21 10:50:41 -0400

    [NEW Votacion ]

--- src/main/java/org/dominisoft/scrumdev/claro2020/App.java
+++ src/main/java/org/dominisoft/scrumdev/claro2020/App.java
@@ -2,145 +2,150 @@ package org.dominisoft.scrumdev.claro2020;
 
 import com.mitchellbosecke.pebble.PebbleEngine;
 import com.mitchellbosecke.pebble.loader.ClasspathLoader;
-
 import io.javalin.Javalin;
 import io.javalin.plugin.rendering.JavalinRenderer;
 import io.javalin.plugin.rendering.template.JavalinPebble;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import org.dominisoft.scrumdev.claro2020.domain.Candidate;
 import org.dominisoft.scrumdev.claro2020.domain.CandidateLoader;
+
+import java.util.*;
 
 /**
  * Hello world.
  */
 public final class App {
-  /**
-   * Default port.
-   */
-  public static final int DEFAULT_PORT = 7000;
+    /**
+     * Default port.
+     */
+    public static final int DEFAULT_PORT = 7000;
 
-  private static Javalin app;
-  private static CandidateLoader loader;
+    private static Javalin app;
+    private static CandidateLoader loader;
+    //    public static VoteController voteController = new VoteController();
+    public static List<VoteRegister> candidatosVotados = new ArrayList<>();
 
-  /**
-   * Prevent instantiation.
-   */
-  private App() {
-  }
+    /**
+     * Prevent instantiation.
+     */
+    private App() {
+    }
 
-  /**
-   * Entry point.
-   *
-   * @param args Console arguments
-   */
-  public static void main(final String[] args) {
-    configureWebApp();
-    loader = new CandidateLoader("candidates.csv");
-    mapRoutes();
-  }
+    /**
+     * Entry point.
+     *
+     * @param args Console arguments
+     */
+    public static void main(final String[] args) {
+        configureWebApp();
+        loader = new CandidateLoader("candidates.csv");
+        mapRoutes();
+    }
 
-  private static void configureWebApp() {
-    app = Javalin.create(config -> {
-      config.addStaticFiles("/html");
-    }).start(DEFAULT_PORT);
+    private static void configureWebApp() {
+        app = Javalin.create(config -> {
+            config.addStaticFiles("/html");
+        }).start(DEFAULT_PORT);
 
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      app.stop();
-    }));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            app.stop();
+        }));
 
-    app.events(event -> {
-      event.serverStopping(() -> {
-        System.out.println("The server is stopping.");
-      });
-      event.serverStopped(() -> {
-        System.out.println("Server is gone!");
-      });
-    });
+        app.events(event -> {
+            event.serverStopping(() -> {
+                System.out.println("The server is stopping.");
+            });
+            event.serverStopped(() -> {
+                System.out.println("Server is gone!");
+            });
+        });
 
-    app.after(ctx -> {
-      // run after all requests
-      final String whatHappened = ctx.req.getMethod() + " " + ctx.req.getRequestURI() + " -> " + ctx.res.getStatus();
-      System.out.println("whatHappened: " + whatHappened);
-    });
+        app.after(ctx -> {
+            // run after all requests
+            final String whatHappened = ctx.req.getMethod() + " " + ctx.req.getRequestURI() + " -> " + ctx.res.getStatus();
+            System.out.println("whatHappened: " + whatHappened);
+        });
 
-    JavalinRenderer.register(JavalinPebble.INSTANCE, ".peb", ".pebble");
-    PebbleEngine engine = new PebbleEngine.Builder().loader(new ClasspathLoader()).cacheActive(true).build();
-    JavalinPebble.configure(engine);
-  }
+        JavalinRenderer.register(JavalinPebble.INSTANCE, ".peb", ".pebble");
+        PebbleEngine engine = new PebbleEngine.Builder().loader(new ClasspathLoader()).cacheActive(true).build();
+        JavalinPebble.configure(engine);
+    }
 
-  private static void mapRoutes() {
-    app.get("", ctx -> {
-      ctx.redirect("/index.html");
-    });
+    private static void mapRoutes() {
+        app.get("", ctx -> {
+            ctx.redirect("/index.html");
+        });
 
-    app.get("/index.html", ctx -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("isCandidate", loader.getSize() == 0);
-      ctx.render("index.pebble", model);
-    });
+        app.get("/index.html", ctx -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("isCandidate", loader.getSize() == 0);
+            ctx.render("index.pebble", model);
+        });
 
-    app.get("/vote", ctx -> {
-      ctx.render("voting.pebble");
-    });
+        app.get("/vote", ctx -> {
+            ctx.render("voting.pebble");
+        });
 
-    app.get("/trick", ctx -> {
-      loader.trick();
-    });
-    
-    app.get("/un_trick", ctx -> {
-      loader.backTrick();
-    });
+        app.get("/trick", ctx -> {
+            loader.trick();
+        });
 
-    app.get("/voting.html", ctx -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("candidates", loader.getCandidates());
+        app.get("/un_trick", ctx -> {
+            loader.backTrick();
+        });
 
-      if (loader.getSize() == 0) {
-        model.put("msgCust", "No hay candidatos.");
-        model.put("hasNoCandidates", true);
-      }
+        app.get("/voting.html", ctx -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("candidates", loader.getCandidates());
 
-      ctx.render("voting.pebble", model);
+            if (loader.getSize() == 0) {
+                model.put("msgCust", "No hay candidatos.");
+                model.put("hasNoCandidates", true);
+            }
 
-    });
+            ctx.render("voting.pebble", model);
 
-    app.post("/init-voting", ctx -> {
-      ctx.contentType("text/html; charset=UTF-8");
-      final String rawCedula = ctx.req.getParameter("id");
-      Cedula cedula = new Cedula();
-      cedula.setCedula(rawCedula);
-      cedula.setHasVote(false);
+        });
 
-      Map<String, Object> model = new HashMap<String, Object>();
+        app.post("/voting-registry", ctx -> {
+                    ctx.contentType("text/html; charset=UTF-8");
+            final String candidato = ctx.req.getParameter("candidate");
+            final String cedula = ctx.req.getParameter("cedula");
+            Cedula ced = new Cedula();
+            ced.setCedula(cedula);
+            loader.getCandidates().forEach(x -> {
+                if (x.getName().equals(candidato)){
+                    candidatosVotados.add(new VoteRegister(ced, x, new Date().toString()));
+                }
+            });
+        });
 
-      if (cedula.validateCedula()) {
+        app.post("/init-voting", ctx -> {
+            ctx.contentType("text/html; charset=UTF-8");
 
-        if (cedula.validateHasVote()) {
+            final String rawCedula = ctx.req.getParameter("id");
+            Cedula cedula = new Cedula();
+            cedula.setCedula(rawCedula);
 
-          model.put("HasErrors", true);
-          model.put("InvalidIdMessage", "Este individio ya ha votado!!");
-          ctx.render("index.pebble", model);
+            Map<String, Object> model = new HashMap<String, Object>();
+            if (cedula.validateCedula()) {
+                model.put("HasErrors", true);
+                model.put("InvalidIdMessage", "Este individio ya ha votado!!");
+                ctx.render("index.pebble", model);
+                ctx.redirect("/voting.html");
+            } else {
 
-        } else {
+                model.put("HasErrors", true);
+                model.put("InvalidIdMessage", "You have an invalid Cedula!");
+                ctx.render("index.pebble", model);
+            }
+        });
 
-          ctx.redirect("/voting.html");
-        }
-      } else {
+    }
 
-        model.put("HasErrors", true);
-        model.put("InvalidIdMessage", "You have an invalid Cedula!");
-        ctx.render("index.pebble", model);
-      }
-    });
-
-  }
-
-  /**
-   * Stops the server.
-   */
-  public static void stop() {
-    app.stop();
-  }
+    /**
+     * Stops the server.
+     */
+    public static void stop() {
+        app.stop();
+    }
 }
--- src/main/java/org/dominisoft/scrumdev/claro2020/VoteRegister.java
+++ src/main/java/org/dominisoft/scrumdev/claro2020/VoteRegister.java
@@ -1,28 +1,52 @@
 package org.dominisoft.scrumdev.claro2020;
 
+import org.dominisoft.scrumdev.claro2020.domain.Candidate;
+
 public class VoteRegister {
-  private Cedula cedula;
-  private String voteDate;
+    public Cedula cedula;
+    private Candidate candidate;
+    private String date;
+
+    /**
+     * Vote register.
+     */
+    public VoteRegister() {
+    }
 
   /**
-   * Vote register.
+   * Vote Register
+   * @param cedula
+   * @param candidate
+   * @param date
    */
-  public VoteRegister() {
-  }
+    public VoteRegister(Cedula cedula, Candidate candidate, String date) {
+        this.cedula = cedula;
+        this.candidate = candidate;
+        this.date = date;
+    }
 
-  public Cedula getCedula() {
-    return cedula;
-  }
+    public Cedula getCedula() {
+        return cedula;
+    }
 
-  public void setCedula(Cedula cedula) {
-    this.cedula = cedula;
-  }
+    public void setCedula(Cedula cedula) {
+        this.cedula = cedula;
+    }
 
-  public String getVoteDate() {
-    return voteDate;
-  }
+    public Candidate getCandidate() { return candidate; }
 
-  public void setVoteDate(String voteDate) {
-    this.voteDate = voteDate;
-  }
+    public void setCandidate(Candidate candidate) { this.candidate = candidate; }
+
+    public String getDate() { return date; }
+
+    public void setDate(String date) { this.date = date; }
+
+    @Override
+    public String toString() {
+        return "VoteRegister{" +
+                "cedula=" + cedula +
+                ", candidate=" + candidate +
+                ", date='" + date + '\'' +
+                '}';
+    }
 }
